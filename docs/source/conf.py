357d33ba5e0d7445131358a5fae3637f6b14851e
Matt Shannon <matthew.j.shannon@gmail.com>
2017-08-05 01:20:15 -0700

    Change theme, add Mock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,18 +22,18 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
 
-# try:
-#     from unittest.mock import MagicMock
-# except ImportError:
-#     from mock import Mock as MagicMock
+try:
+    from unittest.mock import MagicMock
+except ImportError:
+    from mock import Mock as MagicMock
 
-# class Mock(MagicMock):
-#     @classmethod
-#     def __getattr__(cls, name):
-#             return MagicMock()
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return MagicMock()
 
-# MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot', 'numpy', 'pytz', 'timedate', 'scipy']
-# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot', 'numpy', 'pytz', 'timedate', 'scipy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
 # -- General configuration ------------------------------------------------
@@ -102,9 +102,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'alabaster'
 # html_theme = 'default'
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
