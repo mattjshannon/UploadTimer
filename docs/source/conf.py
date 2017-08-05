5198502cd1e3d143883afca37a916c7cecc7a4cf
Matt Shannon <matthew.j.shannon@gmail.com>
2017-08-05 01:44:36 -0700

    matplotlib.dates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot', 'numpy', 'pytz', 'timedate', 'scipy']
+MOCK_MODULES = ['matplotlib', 'matplotlib.pyplot', 'matplotlib.dates', 'numpy', 'pytz', 'timedate', 'scipy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
